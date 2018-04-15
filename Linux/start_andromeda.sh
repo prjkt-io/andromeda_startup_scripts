246e6140d7676094aa6387e99a8561d29372685d
Harsh Shandilya <harsh@prjkt.io>
2018-04-15 17:38:37 +0530

    Linux: Cleanup shellcheck warnings
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- Linux/start_andromeda.sh
+++ Linux/start_andromeda.sh
@@ -7,7 +7,7 @@ echo "Please only have one device connected at a time to use this!"
 echo ""
 
 # Get the current directory of the device running this script
-ROOT=$(dirname ${0})
+ROOT=$(dirname "${0}")
 
 # ADB specific commands for termination
 adb kill-server
@@ -17,32 +17,32 @@ adb start-server
 ADB="adb shell"
 
 # Let's first grab the location where Andromeda is installed
-pkg=$(${ROOT}/${ADB} pm path projekt.andromeda)
+pkg=$("${ROOT}"/"${ADB}" pm path projekt.andromeda)
 
 # Due to the way the output is formatted, we have to strip 10 chars at the start
-pkg=${pkg//package:/}
+pkg="${pkg//package:/}"
 
 # Now let's kill the running Andromeda services on the mobile device
-kill=$(${ROOT}/${ADB} pidof andromeda)
+kill=$("${ROOT}"/"${ADB}" pidof andromeda)
 
 # Check if we need to kill the existing pids, then kill them if need be
 if [[ "${kill}" == "" ]]
 then echo
-${ROOT}/${ADB} << EOF
+"${ROOT}"/"${ADB}" << EOF
 am force-stop projekt.substratum
 appops set projekt.andromeda RUN_IN_BACKGROUND allow
 appops set projekt.substratum RUN_IN_BACKGROUND allow
-CLASSPATH=${pkg} app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
+CLASSPATH="${pkg}" app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
 echo "You can now remove your device from the computer!"
 exit
 EOF
 else echo
-${ROOT}/${ADB} << EOF
+"${ROOT}"/"${ADB}" << EOF
 am force-stop projekt.substratum
-kill -9 ${kill}
+kill -9 "${kill}"
 appops set projekt.andromeda RUN_IN_BACKGROUND allow
 appops set projekt.substratum RUN_IN_BACKGROUND allow
-CLASSPATH=${pkg} app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
+CLASSPATH="${pkg}" app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
 echo "You can now remove your device from the computer!"
 exit
 EOF

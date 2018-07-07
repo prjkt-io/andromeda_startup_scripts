f5444d0b4be52404e2e775f55887d41cfb2caf7e
Harsh Shandilya <harsh@prjkt.io>
2018-07-07 21:21:16 +0530

    Fix support for system adb
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- Linux/start_andromeda.sh
+++ Linux/start_andromeda.sh
@@ -6,9 +6,6 @@ echo "Make sure the device is connected and ADB option enabled"
 echo "Please only have one device connected at a time to use this!"
 echo ""
 
-# Get the current directory of the device running this script
-ROOT=$(dirname "${0}")
-
 # ADB specific commands for termination
 # Don't kill existing servers in case someone has
 # a wireless ADB setup going.
@@ -17,23 +14,23 @@ adb start-server
 
 # Device configuration of the testing rack
 ADB="$(which adb)"
-if [ ${ADB} == "" ]; then
-    ADB="adb"
+if [[ ${ADB} == "" ]]; then
+    ADB="$(pwd)/adb"
 fi
 
 # Let's first grab the location where Andromeda is installed
-pkg=$("${ROOT}"/"${ADB}" shell pm path projekt.andromeda)
+pkg=$("${ADB}" shell pm path projekt.andromeda)
 
 # Due to the way the output is formatted, we have to strip 10 chars at the start
 pkg="${pkg//package:/}"
 
 # Now let's kill the running Andromeda services on the mobile device
-kill=$("${ROOT}"/"${ADB}" shell pidof andromeda)
+kill=$("${ADB}" shell pidof andromeda)
 
 # Check if we need to kill the existing pids, then kill them if need be
 if [[ "${kill}" == "" ]]
 then echo
-"${ROOT}"/"${ADB}" shell << EOF
+"${ADB}" shell << EOF
 am force-stop projekt.substratum
 appops set projekt.andromeda RUN_IN_BACKGROUND allow
 appops set projekt.substratum RUN_IN_BACKGROUND allow
@@ -42,7 +39,7 @@ echo "You can now remove your device from the computer!"
 exit
 EOF
 else echo
-"${ROOT}"/"${ADB}" shell << EOF
+"${ADB}" shell << EOF
 am force-stop projekt.substratum
 kill -9 "${kill}"
 appops set projekt.andromeda RUN_IN_BACKGROUND allow

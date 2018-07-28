4daee459b789bf6489d4e8dcd46616afa848f9d0
Harsh Shandilya <harsh@prjkt.io>
2018-07-28 17:08:43 +0530

    Add script to package things per-platform
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

---
+++ package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+CL_RST="\033[0m"
+CL_YLW="\033[01;33m"
+
+# Some housekeeping never hurt anybody
+git clean -fdx
+
+for platform in Linux macOS; do
+    echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
+    tar -cf andromeda_scripts_"$(git rev-parse --short HEAD)"_"${platform}".tar "${platform}/"
+done
+
+for platform in Windows; do
+    echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
+    zip -r andromeda_scripts_"$(git rev-parse --short HEAD)"_"${platform}".zip "${platform}/" >/dev/null 2>&1
+done

e0fff5c0ba1b3ac679a44c32532319239d4bad22
Harsh Shandilya <harsh@prjkt.io>
2018-08-11 12:54:14 +0530

    Use tags for versioning in package scripts
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- package.sh
+++ package.sh
@@ -8,10 +8,10 @@ git clean -fdx
 
 for platform in Linux macOS; do
     echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
-    tar -cf andromeda_scripts_"$(git rev-parse --short HEAD)"_"${platform}".tar "${platform}/"
+    tar -cf andromeda_scripts_"$(git describe --tags)"_"${platform}".tar "${platform}/"
 done
 
 for platform in Windows; do
     echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
-    zip -r andromeda_scripts_"$(git rev-parse --short HEAD)"_"${platform}".zip "${platform}/" >/dev/null 2>&1
+    zip -r andromeda_scripts_"$(git describe --tags)"_"${platform}".zip "${platform}/" >/dev/null 2>&1
 done

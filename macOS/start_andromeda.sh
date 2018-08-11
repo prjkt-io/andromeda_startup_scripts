e310c6bd9cca1d160fc4e74fb931ad792076a683
Harsh Shandilya <harsh@prjkt.io>
2018-08-11 12:51:38 +0530

    macOS: Small style improvements
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- macOS/start_andromeda.sh
+++ macOS/start_andromeda.sh
@@ -10,7 +10,7 @@ echo " "
 ADB_LOC="./adb"
 
 # check connected device
-CUR_DEVICE=$( $ADB_LOC devices )
+CUR_DEVICE=$("${ADB_LOC}" devices )
 CUR_DEVICE=${CUR_DEVICE:24}
 if [[ "$CUR_DEVICE" == "" ]]
 then
@@ -18,38 +18,38 @@ echo "error: No device detected!"
 echo " "
 echo "The script will now close. (404)"
 sleep 3
-echo "QUITAPP\n"
+echo -e "QUITAPP\n"
 fi
 
 
 # Let's first grab the location where Andromeda is installed
-PKG_LOC=$( $ADB_LOC shell pm path projekt.andromeda )
-if [[ "$PKG_LOC" != "" ]]
+PKG_LOC=$("${ADB_LOC}" shell pm path projekt.andromeda)
+if [[ "${PKG_LOC}" != "" ]]
 then
 echo "Andromeda found! Starting now ..."
-else 
+else
 echo "error: Andromeda not found! Please install Andromeda and try again."
 echo " "
 echo "The script will now close. (414)"
 sleep 3
-echo "QUITAPP\n"
+echo -e "QUITAPP\n"
 fi
 
 # Due to the way the output is formatted, we have to strip 10 chars at the start
-PKG_LOC=$( echo $PKG_LOC | cut -d : -f 2 )
+PKG_LOC=$(echo "${PKG_LOC}" | cut -d : -f 2)
 
 # Now let's kill the running Andromeda services on the mobile device
-KILL=$( $ADB_LOC shell pidof andromeda )
+KILL=$("${ADB_LOC}" shell pidof andromeda)
 
 # Check if we need to kill the existing pids, then kill them if need be
 if [[ "$KILL" == "" ]]
 then
 echo
-$ADB_LOC shell << EOF
+"${ADB_LOC}" shell << EOF
 am force-stop projekt.substratum
 appops set projekt.andromeda RUN_IN_BACKGROUND allow
 appops set projekt.substratum RUN_IN_BACKGROUND allow
-CLASSPATH=$PKG_LOC app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
+CLASSPATH="${PKG_LOC}" app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
 echo "You can now remove your device from the computer!"
 echo " "
 echo "The script will now close. (0)"
@@ -59,12 +59,12 @@ exit
 EOF
 else
 echo
-$ADB_LOC shell << EOF
+"${ADB_LOC}" shell << EOF
 am force-stop projekt.substratum
-kill -9 $KILL
+kill -9 "${KILL}"
 appops set projekt.andromeda RUN_IN_BACKGROUND allow
 appops set projekt.substratum RUN_IN_BACKGROUND allow
-CLASSPATH=$PKG_LOC app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
+CLASSPATH="${PKG_LOC}" app_process /system/bin --nice-name=andromeda projekt.andromeda.Andromeda &
 echo "You can now remove your device from the computer!"
 echo " "
 echo "The script will now close. (0)"

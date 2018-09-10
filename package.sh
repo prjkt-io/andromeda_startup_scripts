e5dfe611083eff8619eec8aee1947778e9796fca
Harsh Shandilya <harsh@prjkt.io>
2018-09-10 16:27:36 +0530

    Pimp out packaging script
    Signed-off-by: Harsh Shandilya <harsh@prjkt.io>

--- package.sh
+++ package.sh
@@ -2,16 +2,19 @@
 
 CL_RST="\033[0m"
 CL_YLW="\033[01;33m"
+CL_BLUE="\033[01;34m"
+VERSION="$(git describe --tags)"
 
 # Some housekeeping never hurt anybody
-git clean -fdx
+git clean -fdx >/dev/null 2>&1
 
+echo -e "${CL_BLUE}Version: ${VERSION}${CL_RST}"
 for platform in Linux macOS; do
     echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
-    tar -cf andromeda_scripts_"$(git describe --tags)"_"${platform}".tar "${platform}/"
+    tar -cf andromeda_scripts_"${VERSION}"_"${platform}".tar "${platform}/"
 done
 
 for platform in Windows; do
     echo -e "${CL_YLW}Packaging for ${platform}...${CL_RST}"
-    zip -r andromeda_scripts_"$(git describe --tags)"_"${platform}".zip "${platform}/" >/dev/null 2>&1
+    zip -r andromeda_scripts_"${VERSION}"_"${platform}".zip "${platform}/" >/dev/null 2>&1
 done
